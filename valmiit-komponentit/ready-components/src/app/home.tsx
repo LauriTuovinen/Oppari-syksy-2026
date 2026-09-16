57949b146e5f68491d7e3d5a2f2685fe01b7f5df
Lauri Tuovinen <lare.tuovinen@gmail.com>
2026-09-16 14:37:28 +0300

    started testing gluestack

---
+++ valmiit-komponentit/ready-components/src/app/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function Home() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Edit src/app/home.tsx to edit this screen.</Text>
+    </View>
+  );
+}
--- valmiit-komponentit/ready-components/src/app/index.tsx
+++ valmiit-komponentit/ready-components/src/app/index.tsx
@@ -1,17 +1,70 @@
-import { Text, View, StyleSheet } from "react-native";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider/index.web";
+import { Text, View } from "react-native";
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+  TabsContentWrapper,
+  TabsTriggerText,
+  TabsIndicator,
+} from '@/components/ui/tabs';
+import {
+  Camera,
+  Home,
+} from 'lucide-react-native';
+import { Icon, EditIcon, FavouriteIcon } from '@/components/ui/icon';
+import { Box } from '@/components/ui/box';
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <View className="flex-1 *:bg-background items-end justify-end">
+        <Tabs defaultValue="home">
+          <TabsList className="rounded-none">
+            <TabsTrigger value="home">
+              <Icon as={EditIcon} size="lg" />
+            </TabsTrigger>
+            <TabsTrigger value="places">
+              <Icon as={EditIcon} size="lg" />
+            </TabsTrigger>
+              <TabsTrigger value="saved">
+              <Icon as={FavouriteIcon} size="lg" />
+            </TabsTrigger>
+            <TabsTrigger value="profile">
+              <Icon as={EditIcon} size="lg" />
+            </TabsTrigger>
+            <TabsIndicator />
+          </TabsList>
+
+          <TabsContentWrapper>
+            <TabsContent value="home">
+              <Box className="p-4">
+                <Icon as={EditIcon} size="lg" />
+                <Text className="text-foreground">Welcome to the Home tab!</Text>
+              </Box>
+            </TabsContent>
+            <TabsContent value="places">
+              <Box className="p-4">
+                <Icon as={EditIcon} size="lg" />
+                <Text className="text-foreground">Your places information</Text>
+              </Box>
+            </TabsContent>            
+            <TabsContent value="saved">
+              <Box className="p-4">
+                <Icon as={FavouriteIcon} size="lg" />
+                <Text className="text-foreground">Your saved items</Text>
+              </Box>
+            </TabsContent>
+            <TabsContent value="profile">
+              <Box className="p-4">
+                <Icon as={EditIcon} size="lg" />
+                <Text className="text-foreground">Your profile information</Text>
+              </Box>
+            </TabsContent>
+          </TabsContentWrapper>
+        </Tabs>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
